0bb97ce4bd831bb7aa5da9231a87ec12bf5c710b
AdarshaGS <adarshgs1928@gmail.com>
2022-09-28 08:09:45 +0530

    LP-6: Update User by id

--- platform/src/main/java/com/website/User/api/UsersApiResource.java
+++ platform/src/main/java/com/website/User/api/UsersApiResource.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -18,7 +19,7 @@ import com.website.User.service.UserService;
 @RestController
 @RequestMapping("users")
 public class UsersApiResource {
-    
+
     @Autowired
     private UserService service;
 
@@ -27,21 +28,26 @@ public class UsersApiResource {
     }
 
     @PostMapping("/create")
-    public CreateUserResponse response(@RequestBody CreateUserPayload payload){
+    public CreateUserResponse response(@RequestBody CreateUserPayload payload) {
         CreateUserResponse response = this.service.createUser(payload);
         return response;
     }
-    
+
     @GetMapping
-    public List<GetUserResponse> response(){
+    public List<GetUserResponse> response() {
         List<GetUserResponse> response = this.service.getAllUsers();
         return response;
     }
 
     @GetMapping("/{id}")
-    public List<GetUserResponse> response(@PathVariable("id") final Long id){
+    public List<GetUserResponse> retrieveUserById(@PathVariable("id") final Long id) {
         List<GetUserResponse> getUserById = this.service.getUserById(id);
         return getUserById;
-        
+    }
+
+    @PutMapping("/update/{id}")
+    public GetUserResponse UpdateUser(@PathVariable("id") final Long id, @RequestBody CreateUserPayload payload) {
+        GetUserResponse updateUser = this.service.update(id, payload);
+        return updateUser;
     }
 }
